8df20755090028361d8ffd7c4c2ca9c2c2a43418
edwingc88 <edwingc88@gmail.com>
2026-03-06 08:14:36 -0400

    feat: responsive mobile menu y mejoras visuales hero

---
+++ script.js
@@ -0,0 +1,10 @@
+/* https://docs.google.com/forms/d/e/1FAIpQLSfVOPXaHe14ftIOEGx7HS7_gitc_b84_ibwqetGlZ8fZr9mZA/viewform?usp=publish-editor */
+
+/* 
+
+https://docs.google.com/forms/u/0/d/1h1Tsi2TcHdEe3d1Xd2M-WrwUyPJpipQtjG-ZVVLTkpE/previewResponse
+
+
+https://docs.google.com/forms/d/e/1FAIpQLSfVOPXaHe14ftIOEGx7HS7_gitc_b84_ibwqetGlZ8fZr9mZA/viewform?usp=pp_url&entry.1826377319=edwin&entry.746695071=0414237587&entry.1332415480=Desarrollo+web&entry.855171741=pagina+wen+
+
+*/
